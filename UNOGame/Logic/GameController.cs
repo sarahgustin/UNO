c5b195435949cd240c972818916a77922be40687
Sarah Gustin <sarahngustine02@gmail.com>
2026-04-22 07:58:20 +0700

    console progress

--- UNOGame/Logic/GameController.cs
+++ UNOGame/Logic/GameController.cs
@@ -1,10 +1,9 @@
-using System;
 using UNOGame.Enums;
 using UNOGame.Models;
 
 namespace UNOGame.Logic;
 
-class GameController
+public class GameController
 {
     //dictionary player dan list kartu di hand
     private readonly Dictionary<IPlayer, List<ICard>> _players;
@@ -12,7 +11,12 @@ class GameController
     private IPlayer _currentPlayer;
     private IDeck _gameDeck;
     private IBoard _gameBoard;
-
+    
+    //buat kasih message ke player kalo mereka dapet kartu draw
+    private string _gameDrawMessage;
+    
+    //event baru buat pilih warna
+    public Func<CardColor> OnRequestColorSelection ;
    public event Action OnDeckEmpty = delegate { }; 
 
    public delegate void GameEndHandler(IPlayer winner);
@@ -31,14 +35,43 @@ class GameController
         foreach (var player in players)
         {
             _players.Add(player, new List<ICard>());
+
+            //masukin kartu ke tangan, 7 kartu untuk mulai permainan
+            for (int i = 0; i < 7; i++)
+            {
+                _players[player].Add(DrawCard(deck));
+            }
         }
-
         _currentPlayer = players[0];
-
         _isClockWise = true;
+        _gameBoard.UsedCards.Add(DrawCard(deck));
+
     }
 
-    public List<IPlayer> GetPlayerList(){//untuk ambil data pemain dari list player, key pemain untuk dapat mengakses kartu 
+    //bikin method baru tambah kartu ke list card 
+    /*
+    public static List<ICard> GenerateFullDeck(List<ICard> cards)
+    {
+        List<ICard> fullDeck = new List<ICard>();
+        CardColor[] cardColor = { CardColor.Red, CardColor.Blue, CardColor.Green, CardColor.Yellow };
+
+        foreach (var color in cardColor)
+        {
+            //masukin kartu warna 0, 1 kartu perwarna
+            fullDeck.Add(new Card(color, CardType.Zero));
+            
+            //masukin kartu 1-9 untuk tiap warna
+            for (int i = 1; i <= 9; i++)
+            {
+                fullDeck.Add(new Card(i, color, (CardType)i));
+                fullDeck.Add(new Card(i, color, (CardType)i));
+            }
+        }
+    }
+*/
+
+    //untuk ambil data pemain dari list player, key pemain untuk dapat mengakses kartu
+    public List<IPlayer> GetPlayerList(){ 
         var playersKeys = _players.Keys;
 
         List<IPlayer> players = playersKeys.ToList();
@@ -46,7 +79,6 @@ class GameController
         return players;
 
     }//mengembalikan value List<Player 
-   
 
     public List<ICard> GetCurrentPlayerHand() => _players[_currentPlayer];
     public IPlayer GetCurrentPlayer() => _currentPlayer;
@@ -83,8 +115,26 @@ class GameController
     public IDeck GetDeck() =>_gameDeck;
     public IBoard GetBoard() =>_gameBoard;
     public bool GetIsClockwise() => _isClockWise;
-    
 
+    public void LogDrawActivity(IPlayer player, int count, string reason)
+    {
+        if (count == 1)
+        {
+            _gameDrawMessage = $"{player.Name} {reason}, mendapatkan {count} kartu draw";
+        }
+        else
+        {
+            _gameDrawMessage = $"{player.Name} kena {reason}, mendapatkan {count} kartu!";
+        }
+    }
+    
+    public string GetLatestActivity() 
+    {
+        string temp = _gameDrawMessage;
+        _gameDrawMessage = null; // Dihapus setelah diambil biar gak muncul terus
+        return temp;
+    }
+    
     /*Card Behavior*/
     private void ReverseDirection()
     {
@@ -95,7 +145,7 @@ class GameController
         _currentPlayer = GetNextPlayer();
     }
 
-    private void Wild(CardColor newColor)
+    public void Wild(CardColor newColor)
     {
         //ambil top card yang ada di board
         ICard topCard = GetTopCard(_gameBoard);
@@ -108,6 +158,7 @@ class GameController
             DrawPenalty(4, _gameDeck, GetNextPlayer());
 
             SkipNextPlayer();
+            LogDrawActivity(GetNextPlayer(), 4, "Draw Penalty +4");
         }
 
         _currentPlayer = GetNextPlayer();
@@ -189,45 +240,45 @@ class GameController
         ShuffleDeck(deck);
     }
     
-    public void PlayerTurn(IBoard board)
+    public void PlayerTurn(IBoard board, int? choice = null)
     {
         //current player
         IPlayer currentPlayer = _currentPlayer;
         //getcurrent hand
         List<ICard> playerHand = _players[currentPlayer];
-        GetTopCard(board);
         
         //cek di tangan ada kartu yang valid atau ngga
-        bool canPlay = false;
-        ICard selectedCard = null;
+        bool canPlay = playerHand.Any(card => IsPlaceableOnTop(card, board));
 
-        foreach (var card in playerHand)
-        {
-            if (IsPlaceableOnTop(card, board))
-            {
-                canPlay = true;
-                selectedCard = card;
-            }
-        }
 
-        if (canPlay)
-        {
-            PlacedCard(selectedCard, board);
-        }
-        else
+        if (!canPlay)
         {
             //panggil drawcard untuk ambi 1 kartu ketika kartu ditangan ngga ada yg valid
             ICard newCard = DrawCard(_gameDeck);
             //tambah 1 kartu ke tangan
             playerHand.Add(newCard);
-            
-            //cek lagi kartu yang udah di ambil di cek lagi valid ngga untuk PlaceCard
+            LogDrawActivity(currentPlayer, 1, "Gak ada kartu cocok");
+
             if (IsPlaceableOnTop(newCard, board))
             {
                 PlacedCard(newCard, board);
             }
+            _currentPlayer = GetNextPlayer();
+
         }
-        _currentPlayer = GetNextPlayer();
+        else if(choice.HasValue)
+        {
+            int index = choice.Value - 1; 
+            if (index >= 0 && index < playerHand.Count)
+            {
+                ICard seletectedCard = playerHand[index];
+                if (IsPlaceableOnTop(seletectedCard, board))
+                {
+                    PlacedCard(seletectedCard, board);
+                }
+            }
+        }
+
     }
 
     public void PlacedCard(ICard card, IBoard board)
@@ -256,23 +307,27 @@ class GameController
             }else if (card.CardType == CardType.Draw)
             {
                 DrawPenalty(2, _gameDeck, GetNextPlayer());
+                LogDrawActivity(GetNextPlayer(), 2, "Penalty +2");
                 SkipNextPlayer();
             }
             _currentPlayer = GetNextPlayer();
         }
-        //kalo cardtype wild/wilddraw panggil UI untuk pilih color
+        //kalo cardtype wild/wilddraw panggil UI untuk pilih color. PAKE EVENT
         else
         {
-            //UI pilih warna
+            if (OnRequestColorSelection != null)
+            {
+                CardColor chosenColor = OnRequestColorSelection.Invoke();
+                Wild(chosenColor);
+            }
 
         }
         
         //if player handcount udah abis panggil EndGame()
-        if (_players[_currentPlayer].Count == 0)
+        if (_players[currentPlayer].Count == 0)
         {
-            GameEnd(_currentPlayer);
+            GameEnd(currentPlayer);
             return;
-           
         }
     }
     public bool IsPlaceableOnTop(ICard card, IBoard board)
--- UNOGame/UI/ConsoleDisplay.cs
+++ UNOGame/UI/ConsoleDisplay.cs
@@ -1,6 +1,169 @@
+using UNOGame.Models;
+using UNOGame.Logic;
+using UNOGame.Enums;
+
 namespace UNOGame.UI;
 
 public class ConsoleDisplay
 {
+    //show menu
+    public static void ShowMenu()
+    {
+        Console.WriteLine("");
+        Console.WriteLine("==================================");
+        Console.WriteLine("   WELCOME TO UNO CONSOLE GAME!");
+        Console.WriteLine("==================================");
+        Console.WriteLine("1. Start Game");
+        Console.WriteLine("2. Exit");
+        Console.WriteLine("Masukkan Pilihan : ");
+        int pilihan = Convert.ToInt32(Console.ReadLine());
+        
+        switch (pilihan)
+        {
+            case 1 :  
+                InitPlayer();
+                break;
+            case 2 :
+                Environment.Exit(0);
+                break;
+            default : 
+                Console.WriteLine("Pilihan Tidak Valid!");
+                break;
+        }
+    }
+    //initialize player
+    public static List <IPlayer> InitPlayer()
+    {
+        List<IPlayer> players = new List<IPlayer>();
+        
+        
+        Console.WriteLine("Masukkan Jumlah Player (2-4): ");
+        int.TryParse(Console.ReadLine(), out int count);
+
+        if (count < 2) count = 2;
+        if (count > 4) count = 4;
+        
+        //masukin player yang assgin ke list player.
+        for (int i = 1; i <= count; i++)
+        {
+            Console.WriteLine($"Masukan Nama Player ke{i} : ");
+            string name = Console.ReadLine()??"";
+            
+            players.Add(new Player(name));
+        }
+        return players;
+    }
     
+    //showheader
+    public void ShowHeader(GameController gc, IBoard board)
+    {
+        //gettopcard
+        ICard topCard = gc.GetTopCard(board);
+        //getcurrentPlayer
+        IPlayer currentPlayer = gc.GetCurrentPlayer();
+        //GetClockIsWise
+        bool isClockwise = gc.GetIsClockwise();
+        
+        Console.WriteLine("============================");
+        Console.WriteLine("       UNO CONSOLE GAME");
+        Console.WriteLine("=============================");
+        Console.WriteLine($"TOP CARD : [{topCard.CardColor}{topCard.CardType}]");
+        Console.WriteLine("=============================");
+        Console.WriteLine($"GILIRAN : [{currentPlayer.Name}]");
+        Console.WriteLine($" ARAH MAIN: {(isClockwise ? "Searah Jarum Jam (Clockwise)" : "Berlawanan Jarum Jam (Counter-Clockwise)")}");
+    }
+    
+    //show player stats
+    public void ShowPlayerStats(GameController gc)
+    {
+        //getplayerlist
+        List<IPlayer> players = gc.GetPlayerList();
+        
+        //getplayerhand.count
+        foreach (var p in players)
+        {
+            int cardCount = gc.PlayerHandCount(p);
+            
+            Console.WriteLine($"{p.Name.PadRight(15)} : {cardCount}");
+        }
+    }
+    
+    //show player hand card
+    public void ShowHand(GameController gc)
+    {
+        //getplayerhand.currentplayer
+        List<ICard> playerCards = gc.GetCurrentPlayerHand();
+        
+        Console.WriteLine("Kartu Kamu : ");
+        //bikin tampilan piliaan kartu pake nomor
+        for (int i = 0; i < playerCards.Count; i++)
+        {
+            ICard currentCard = playerCards[i];
+            Console.WriteLine($"{i+1}. [{currentCard.CardType} {currentCard.CardColor}] ");
+        }
+    }
+    public static int GetPlayerChoice(int handCount)
+    {
+        //bikin conditonal dari pilihan player, passing ke playerturn
+        while (true)
+        {
+            Console.Write($"\nPilih nomor kartu (1-{handCount}): ");
+            if (int.TryParse(Console.ReadLine(), out int choice) && choice >= 1 && choice <= handCount)
+            {
+                return choice;
+            }
+            Console.WriteLine("Input salah! Masukkan angka sesuai nomor kartu.");
+        }
+    }
+    //show message
+    public static void ShowMessage(string message)
+    {
+        // Biar keliatan beda sama tulisan kartu, kita kasih hiasan dikit
+        Console.WriteLine("\n==========================================");
+        Console.WriteLine($"   [!] INFO: {message}");
+        Console.WriteLine("==========================================");
+        Console.WriteLine("Tekan [Enter] untuk lanjut...");
+        Console.ReadLine(); 
+    }
+    //show color pick
+    public static CardColor ShowColorPick (GameController gc)
+    {
+        Console.WriteLine("Pilih Warna");
+        Console.WriteLine("1. Red");
+        Console.WriteLine("2. Blue");
+        Console.WriteLine("3. Green");
+        Console.WriteLine("4. Yellow");
+        Console.WriteLine("Masukkan Pilihan : ");
+        int input = Convert.ToInt32(Console.ReadLine());
+
+        return input switch
+        {
+            1 => CardColor.Red,
+            2 => CardColor.Blue,
+            3 => CardColor.Green,
+            4 => CardColor.Yellow,
+            _ => CardColor.Red
+        };
+    }
+    
+    //even handler
+    public static void ShowDeckEmptyMessage()
+    {
+        Console.WriteLine("============================");
+        Console.WriteLine("Kartu Deck Habis!! Mengocok ulang kartu di meja");
+        Console.WriteLine("Tekan Enter untuk lanjut...");
+        Console.ReadLine();
+    }
+
+    public static void ShowWinnerAnnouncement(IPlayer winner)
+    {   
+        Console.WriteLine("************************************************");
+        Console.WriteLine($"\nSELAMAT {winner.Name} ADALAH PEMENANGNYA!");
+        Console.WriteLine("       Semua kartu di tangan sudah habis.");
+        Console.WriteLine("************************************************");
+        Console.WriteLine("Game Selesai! Tekan Enter untuk keluar.");
+        Console.ReadLine();
+    }
+
+
 }
